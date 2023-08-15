b0060faa8c1190e1e773396660ba2407cf4e0e16
lucasPereiraEsteves <luccafuso2017@gmail.com>
2023-08-15 15:55:58 -0400

    commit 15_08
    atividade feita

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,10 +1,32 @@
 'use server'
 
-const getUserAuthenticated = (user) => {
+const users = [
+    {
+        nome:'Lucca', email:'luccao@gmail.com', password:'luccao', token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    },
+    {
+        nome:'Kauani', email:'kakau@gmail.com', password:'kakau', token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    },
+    {
+        nome:'Mariana', email:'mari@gmail.com', password:'mari', token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    },
+    {
+        nome:'Will', email:'willwill@gmail.com', password:'willwill', token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    }
+]
 
+const getUserAuthenticated = (users, user) => {
+    for (let i = 0; i < users.length; i++) {
+        if(users[i].email === user.email && users[i].password === user.password) {
+            return true;
+        }
+    }
+    return false;
 }
 
-const getUsers = () =>{
-        
+const getUsers = (user) =>{
+    const getUserAuthenticated = users.find(u => u.email === user.email && u.password === user.password);
+    return getUserAuthenticated;
 }
+
 export { getUsers, getUserAuthenticated };
