1045f061215f6d9c421cb1d2d08dc0cdab920f45
Lucca Paschoalim <luccafuso2017@gmail.com>
2023-09-18 20:40:08 -0400

    update page.js from dashboard

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,13 +1,22 @@
+'useserve'
 import { getUsers } from "@/app/functions/handlerAcessAPI";
+import Navbar from "../../components/Navbar";
+import './dashboard.css'
 
 export default async function Dashboard() {
     const users = await getUsers();
     return (
         <div>
-
+            <Navbar />
             <h1>Dashboard</h1>
-
-            users.map{<div>user.name</div>}
+            <div className="gridUser">
+                {users?.map((user, index) =>
+                    <div key={index} className="user-card">
+                        <h2>{user.name}</h2>
+                        <p>{user.email}</p>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
