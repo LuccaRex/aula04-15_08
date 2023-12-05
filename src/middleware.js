fb661f3c948437fa5577e04d4414f67a16a94f62
LuccaRex <luccafuso2017@gmail.com>
2023-12-05 14:28:32 -0400

    commit final
    ACABOUUU

--- src/middleware.js
+++ src/middleware.js
@@ -6,24 +6,50 @@ export const middleware = async (request) => {
 
     const token = request.cookies.get('token')?.value;
     const urlLogin = new URL('/', request.url);
-    const urlDash = new URL('/pages/dashboard', request.url)
+    const urlDash = new URL('/pages/dashboard', request.url);
+    const urlRegistrar = new URL('/pages/register', request.url)
+    const urlAlterar = new URL('/pages/alter', request.url)
     const isTokenValidated = await validateToken(token);
 
-    if (!isTokenValidated || !token) {
+      if (!isTokenValidated || !token) {
         if (request.nextUrl.pathname === '/pages/dashboard') {
             return NextResponse.redirect(urlLogin);
         }
     }
 
-    if (isTokenValidated) {
+    if (!isTokenValidated || !token) {
+        if (request.nextUrl.pathname === '/pages/register') {
+            return NextResponse.redirect(urlLogin);
+        }
+    }
+
+    if (!isTokenValidated || !token) {
+        if (request.nextUrl.pathname === '/pages/alter') {
+            return NextResponse.redirect(urlLogin);
+        }
+    }
+    
+    if (isTokenValidated || token) {
         if (request.nextUrl.pathname === '/') {
             return NextResponse.redirect(urlDash);
+        }
+    }
+
+    if (isTokenValidated || token) {
+        if (request.nextUrl.pathname === '/') {
+            return NextResponse.redirect(urlRegistrar);
+        }
+    }
+
+    if (isTokenValidated || token) {
+        if (request.nextUrl.pathname === '/') {
+            return NextResponse.redirect(urlAlterar);
         }
     }
 
  NextResponse.next();
 };
 export const config = {
-    matcher: ['/', '/pages/dashboard']
+    matcher: ['/', '/pages/dashboard', '/pages/register', '/pages/alter']
 };
 
